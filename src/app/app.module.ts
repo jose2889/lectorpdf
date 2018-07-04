daa828f50bed689937e9e4e8925467a22b2c8526
Jose Hernandez <jhernandez@gruposolux.com>
2018-07-04 10:42:00 -0400

    se agrego campo para buscar texto en un documento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {
   MatButtonModule, MatFormFieldModule, MatInputModule, MatSlideToggleModule,
-  MatToolbarModule
+  MatToolbarModule, MatTableDataSource
 } from '@angular/material';
+import {MatTableModule} from '@angular/material/table';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { PdfViewerModule } from './pdf-viewer/pdf-viewer.module';
 import { AppComponent } from './app.component';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,6 +26,8 @@ import { AppComponent } from './app.component';
     MatFormFieldModule,
     MatSlideToggleModule,
     MatToolbarModule,
+    MatTableModule,
+    MatIconModule,
 
     PdfViewerModule
   ],
